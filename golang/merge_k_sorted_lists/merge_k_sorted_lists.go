b89d9adc5e27f5edf3a7a1df83aea1a6c3dcfcd8
sangam14 <sbiradar@ced.alliance.edu.in>
2020-10-24 09:19:27 +0530

    Create merge_k_sorted_lists.go

---
+++ golang/merge_k_sorted_lists/merge_k_sorted_lists.go
@@ -0,0 +1,33 @@
+package merge_k_sorted_lists
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	} else if len(lists) == 1 {
+		return lists[0]
+	}
+	return mergeTwoLists(mergeKLists(lists[:len(lists)/2]), mergeKLists(lists[len(lists)/2:]))
+
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+	var head *ListNode
+	if l1.Val < l2.Val {
+		head = l1
+		head.Next = mergeTwoLists(l1.Next, l2)
+	} else {
+		head = l2
+		head.Next = mergeTwoLists(l2.Next, l1)
+	}
+	return head
+}
